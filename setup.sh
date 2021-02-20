ee8f8d74e9f496be3ba1411679b8ea4b781fc684
Carter <if is_empty $emailId; then>
2021-02-20 03:48:59 -0500

    inline helpers

--- setup.sh
+++ setup.sh
@@ -3,8 +3,92 @@
 # Run without downloading by running in your home directory:
 # curl https://raw.githubusercontent.com/CarterMcAlister/dotfiles/master/setup.sh | bash
 
-## Custom color codes & utility functions
-source helper/utility.sh
+# Color Codes & Custom utilities
+# Source: http://natelandau.com/bash-scripting-utilities/
+
+## Fonts
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+## Colors
+purple=$(tput setaf 171)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+tan=$(tput setaf 3)
+blue=$(tput setaf 38)
+
+## To check input is empty or not
+is_empty() {
+if [ $# -eq  0 ]
+  then
+    return 1
+fi
+  return 0
+}
+
+## To check programs exit or not
+is_exists() {
+if [ $(type -P $1) ]; then
+  return 1
+fi
+  return 0
+}
+
+## To check file exits or not
+is_file_exists() {
+if [ -f "$file" ]
+then
+	return 1
+else
+	return 0
+fi
+}
+
+# Custom echo functions
+
+ask() {
+  printf "\n${bold}$@${reset}"
+}
+
+e_thanks() {
+  printf "\n${bold}${purple}$@${reset}\n"
+}
+
+e_header() {
+  printf "\n${underline}${bold}${green}%s${reset}\n" "$@"
+}
+
+e_arrow() {
+  printf "\n ᐅ $@\n"
+}
+
+e_success() {
+  printf "\n${green}✔ %s${reset}\n" "$@"
+}
+
+e_error() {
+  printf "\n${red}✖ %s${reset}\n" "$@"
+}
+
+e_warning() {
+  printf "\n${tan}ᐅ %s${reset}\n" "$@"
+}
+
+e_underline() {
+  printf "\n${underline}${bold}%s${reset}\n" "$@"
+}
+
+e_bold() {
+  printf "\n${bold}%s${reset}\n" "$@"
+}
+
+e_note() {
+  printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@"
+}
+
+## End utilities
+
 
 # Welcome msg
 
@@ -39,7 +123,7 @@ fi
 
 ## Get dotfiles
 e_header "Getting dotfiles"
-git clone git@github.com:$githubUsername/dotfiles.git "${HOME}/dotfiles"
+git clone git@github.com:CarterMcalister/dotfiles.git "${HOME}/dotfiles"
 cp ~/dotfiles/bin_scripts ~/.bin_scripts 
 
 
@@ -137,10 +221,10 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ## Set system configs
-source osx/screen.sh
-source osx/dock.sh
-source osx/system.sh
-source osx/browser.sh
+source ~/dotfiles/osx/screen.sh
+source ~/dotfiles/osx/dock.sh
+source ~/dotfiles/osx/system.sh
+source ~/dotfiles/osx/browser.sh
 
 # 9. Wrap up
 
